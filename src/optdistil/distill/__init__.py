e62c00e28e114d4885f5063ac08841af6e11de3e
yuubinnkyoku <137483288+yuubinnkyoku@users.noreply.github.com>
2026-09-09 13:01:42 +0900

    Export distillation API

--- src/optdistil/distill/__init__.py
+++ src/optdistil/distill/__init__.py
@@ -1,1 +1,23 @@
-"""Trajectory datasets, imitation losses, and optimizer distillation loops."""
+"""Trajectory collection and optimizer distillation."""
+
+from optdistil.distill.collect import collect_teacher_step
+from optdistil.distill.features import FEATURE_NAMES, build_elementwise_features
+from optdistil.distill.losses import (
+    DistillationLossWeights,
+    direction_loss,
+    distillation_loss,
+    magnitude_loss,
+)
+from optdistil.distill.trajectory import TrajectoryDataset, TrajectoryRecord
+
+__all__ = [
+    "FEATURE_NAMES",
+    "DistillationLossWeights",
+    "TrajectoryDataset",
+    "TrajectoryRecord",
+    "build_elementwise_features",
+    "collect_teacher_step",
+    "direction_loss",
+    "distillation_loss",
+    "magnitude_loss",
+]
