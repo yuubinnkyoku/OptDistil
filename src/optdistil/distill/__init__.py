2f57855f9d5aa55c8536196c34df19b46c4d25be
yuubinnkyoku <137483288+yuubinnkyoku@users.noreply.github.com>
2026-09-09 16:20:13 +0900

    Export matrix-aware feature builder

--- src/optdistil/distill/__init__.py
+++ src/optdistil/distill/__init__.py
@@ -1,7 +1,12 @@
 """Trajectory collection and optimizer distillation."""
 
 from optdistil.distill.collect import collect_teacher_step
-from optdistil.distill.features import FEATURE_NAMES, build_elementwise_features
+from optdistil.distill.features import (
+    FEATURE_NAMES,
+    MATRIX_FEATURE_NAMES,
+    build_elementwise_features,
+    build_matrix_aware_features,
+)
 from optdistil.distill.losses import (
     DistillationLossWeights,
     direction_loss,
@@ -18,11 +23,13 @@ from optdistil.distill.trajectory import TrajectoryDataset, TrajectoryRecord
 
 __all__ = [
     "FEATURE_NAMES",
+    "MATRIX_FEATURE_NAMES",
     "DistillationLossWeights",
     "RolloutResult",
     "TrajectoryDataset",
     "TrajectoryRecord",
     "build_elementwise_features",
+    "build_matrix_aware_features",
     "collect_teacher_step",
     "collect_teacher_trajectory",
     "direction_loss",
