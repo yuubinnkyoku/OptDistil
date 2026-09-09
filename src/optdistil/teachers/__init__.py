536356c0a1162c3d22ade37846de33854c3df989
yuubinnkyoku <137483288+yuubinnkyoku@users.noreply.github.com>
2026-09-09 13:01:28 +0900

    Export teacher adapters

--- src/optdistil/teachers/__init__.py
+++ src/optdistil/teachers/__init__.py
@@ -1,1 +1,6 @@
-"""Teacher optimizer adapters and trajectory producers."""
+"""Teacher optimizer adapters."""
+
+from optdistil.teachers.adamw import AdamWTeacher
+from optdistil.teachers.muon import MuonTeacher, zeropower_via_newton_schulz5
+
+__all__ = ["AdamWTeacher", "MuonTeacher", "zeropower_via_newton_schulz5"]
