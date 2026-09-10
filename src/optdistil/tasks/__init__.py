a3353ff9b0776ee91be0dddc84a2ef897663c957
yuubinnkyoku <137483288+yuubinnkyoku@users.noreply.github.com>
2026-09-10 15:25:52 +0900

    Export frozen-readout MLP task

--- src/optdistil/tasks/__init__.py
+++ src/optdistil/tasks/__init__.py
@@ -1,1 +1,5 @@
 """Inner-loop optimization tasks used to train and evaluate optimizers."""
+
+from optdistil.tasks.frozen_readout_mlp import FrozenReadoutMLPTask, make_frozen_readout_mlp
+
+__all__ = ["FrozenReadoutMLPTask", "make_frozen_readout_mlp"]
