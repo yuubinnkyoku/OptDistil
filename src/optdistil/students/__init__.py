489d9fd1c8a59b50e8435a7dfc1dec0568db2a71
yuubinnkyoku <137483288+yuubinnkyoku@users.noreply.github.com>
2026-09-10 10:50:17 +0900

    Export block-gain student

--- src/optdistil/students/__init__.py
+++ src/optdistil/students/__init__.py
@@ -1,5 +1,16 @@
 """Tiny learned optimizer students."""
 
+from optdistil.students.block_gain import (
+    BLOCK_GAIN_FEATURE_NAMES,
+    BlockGainOptimizer,
+    build_block_gain_features,
+)
 from optdistil.students.tiny_mlp import StudentState, TinyMLPOptimizer
 
-__all__ = ["StudentState", "TinyMLPOptimizer"]
+__all__ = [
+    "BLOCK_GAIN_FEATURE_NAMES",
+    "BlockGainOptimizer",
+    "StudentState",
+    "TinyMLPOptimizer",
+    "build_block_gain_features",
+]
