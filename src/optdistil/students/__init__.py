865662277b0f3fddd20a271df08b8dadde8f9ee9
yuubinnkyoku <137483288+yuubinnkyoku@users.noreply.github.com>
2026-09-10 15:13:31 +0900

    Export row-column gain student

--- src/optdistil/students/__init__.py
+++ src/optdistil/students/__init__.py
@@ -5,12 +5,20 @@ from optdistil.students.block_gain import (
     BlockGainOptimizer,
     build_block_gain_features,
 )
+from optdistil.students.row_col_gain import (
+    ROW_COL_GAIN_FEATURE_NAMES,
+    RowColGainOptimizer,
+    build_row_col_gain_features,
+)
 from optdistil.students.tiny_mlp import StudentState, TinyMLPOptimizer
 
 __all__ = [
     "BLOCK_GAIN_FEATURE_NAMES",
+    "ROW_COL_GAIN_FEATURE_NAMES",
     "BlockGainOptimizer",
+    "RowColGainOptimizer",
     "StudentState",
     "TinyMLPOptimizer",
     "build_block_gain_features",
+    "build_row_col_gain_features",
 ]
